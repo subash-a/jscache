810611613db2dd1d93ed7bba7df738c6d3fa3afe
Subhash A <s.a@sap.com>
2013-12-08 12:10:55 +0530

    Added chunked call and templating mechanism, testing the chunking with sample dat set, will add configuration later.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 function start_cache() {
     var cache1 = new Cache();
     //cache1.log(true);
-    cache1.getCacheObject("data/xxxl.json",function(){});
+/*    cache1.getCacheObject("data/xxxl.json",function(){});
     cache1.getCacheObject("data/xxl.json",function(){});
     cache1.getCacheObject("data/xl.json",function(){});
     cache1.getCacheObject("data/large.json",function(){});
     cache1.getCacheObject("data/medium.json",function(){});
-    cache1.getCacheObject("data/small.json",function(){});
+    cache1.getCacheObject("data/small.json",function(){});*/
+    cache1.chunkData("data/chunk{value}.json",[1,2,3,4],"data/chunk.json",0);
 }
