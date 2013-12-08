bad6d989b82cd6c9d987648d7b2029a9bc229591
Subhash A <s.a@sap.com>
2013-12-08 20:28:49 +0530

    Corrected documentation

--- cache.js
+++ cache.js
@@ -36,11 +36,11 @@ This method is used to clear the cache table and empty it in one go.
 	cache_obj = Object.create({"size":0,"data":{},"fetched":1,"last_fetched":null});
     },
 /**
-Method is used for checking the cache and firing a ajax call if the data for that hash id is not found in the cache. It also has the task of updating the cache statistics and cacheObject statistics when the object is found in the cache. It also does the special function of queueing requests for a particular cache object if the id has already been sent via an AJAX request and once the data returns the callbacks are executed in the order they arrived.
-
+Method is used for checking the cache and firing a ajax call if the data for that hash id is not found in the cache. It also has the task of updating the cache statistics and cacheObject statistics when the object is found in the cache. It also does the special function of queueing requests for a particular cache object if the id has already been sent via an AJAX request and once the data returns the callbacks are executed in the order they arrived
+.
 @method checkCache
-@param String id is the cache object hash id (URL and params)
-@param Function callback is the callback to be exected when the object data is available.
+@param {String} id is the cache object hash id (URL and params)
+@param {Function} callback is the callback to be exected when the object data is available.
 **/
     checkCache = function(id,callback) {
 	if(table[id]) {
@@ -66,7 +66,7 @@ Method is used for checking the cache and firing a ajax call if the data for tha
 This method is used for updating cache object flags or statistics so that when the objects are sent for an evaluation by a replacement algorithm their flag values decide whether they will be removed or retained in the cache.
 
 @method updateCacheObjectFlags
-@param String id is the hash id of the cacheObject (URL and params)
+@param {String} id is the hash id of the cacheObject (URL and params)
 **/
     updateCacheObjectFlags = function(id) {
 	if(log){console.log("Updating Cache Object flags")}
@@ -77,8 +77,8 @@ This method is used for updating cache object flags or statistics so that when t
 Method is used for fetching cache object and calls another function internally called checkCache which checks and executes the callback on the cacheObject
 
 @method getCacheObject
-@param String id is the hash id of the cache object (URL and params)
-@return Function checkCache which executes the callback with cacheObject
+@param {String} id is the hash id of the cache object (URL and params)
+@return {Function} checkCache which executes the callback with cacheObject
 **/
     getCacheObject = function (id,callback) {
 	if(log){console.log("Checking for requested Cache Object")}
@@ -88,9 +88,9 @@ Method is used for fetching cache object and calls another function internally c
 The method is used primarily to set a cacheObject manually if one does not want to make a AJAX call via getObject. Must be used in emergency cases only.
 
 @method setCacheObject
-@param String id is the hash id of the cache object (URL and params)
-@param Object obj is the cache object template which contains flags and data value set to a particular value.
-@return Object CACHE is the cache instance which is being set with data.
+@param {String} id is the hash id of the cache object (URL and params)
+@param {Object} obj is the cache object template which contains flags and data value set to a particular value.
+@return {Object} CACHE is the cache instance which is being set with data.
 **/
     setCacheObject = function (id,obj) {
 	if(log){console.log("Setting Cache Object through manual feed")}
@@ -101,8 +101,8 @@ The method is used primarily to set a cacheObject manually if one does not want 
 This method is used for calculating the approximate size of the cache object as a whole along with nseted object structure it returns the size in Bytes. It recursively calculates the size of a nested object.
 
 @method getSize
-@param Object obj is the object for which the size needs to be calculated
-@return Number s is the size of the object in Bytes
+@param {Object} obj is the object for which the size needs to be calculated
+@return {Number} s is the size of the object in Bytes
 **/
     getSize = function(obj) {
 	var sum = 0,
@@ -131,8 +131,8 @@ This method is used for calculating the approximate size of the cache object as 
  Function calculates the size of a given object by passing it to an other function called getSize. This methos is used in case of any pre processing to be doone on the object or parsing purposes
 
 @method calculateSize
-@param Object obj
-@return Function getSize which returns the size of the object passed to it
+@param {Object} obj
+@return {Function} getSize which returns the size of the object passed to it
 **/
     calculateSize = function (obj) {
 	if(log){console.log("Calculating Size of the Object")}
@@ -142,8 +142,8 @@ This method is used for calculating the approximate size of the cache object as 
 Method returns the size of an individual cache object when passed to it by id.
 
 @method getCacheObjectSize
-@param String id is the hash id of the cach object to be used for size calculation
-@return Function calculateSize function is used for calculating size of any object after pre processing.
+@param {String} id is the hash id of the cach object to be used for size calculation
+@return {Function} calculateSize function is used for calculating size of any object after pre processing.
 **/
     getCacheObjectSize = function (id) {
 	return calculateSize(table[id].data);
@@ -152,7 +152,7 @@ Method returns the size of an individual cache object when passed to it by id.
 This method is used for returning the cacheSize of the overall cache when called. This maintains a size varaible which gets updated when any new object is added to the cache and when any object is removed from the cache
 
 @method getCacheSize
-@return Number size in Bytes
+@return {Number} size in Bytes
 **/
     getCacheSize = function () {
 	return size;
@@ -161,7 +161,7 @@ This method is used for returning the cacheSize of the overall cache when called
 This method sets logging as true or false
 
 @method setLogging
-@param Boolean l is the log flag either true or false
+@param {Boolean} l is the log flag either true or false
 **/
     setLogging = function(l){
 	log = l;
@@ -170,7 +170,7 @@ This method sets logging as true or false
 This method is used for fetching the cache statistics of number of items, minimum number of fetches done for an object, cache hits, cache misses and cache size.
 
 @method getStats
-@return Object containing all the above described elements
+@return {Object} containing all the above described elements
 **/
     getStats = function () {
 	return {"items":Object.keys(table).length,"minimum_fetched":minFetched,"cache_size":size,"cache_miss":cacheMisses,"cache_hits":cacheHits};
@@ -179,8 +179,8 @@ This method is used for fetching the cache statistics of number of items, minimu
 This method is used for setting the maximum cache size in bytes that can be used and when the cache size is about to exceed, one of the many algorithms comes into play to replace the cahce object based on some criteria.
 
 @method setMaximumCacheSize
-@param Number sz is the size based on which the criteria is chosen
-@return Object CACHE is the instance of this object
+@param {Number} sz is the size based on which the criteria is chosen
+@return {Object} CACHE is the instance of this object
 **/
     setMaximumCacheSize = function (sz) {
 	if(log){console.log("Setting Maximum Cache Size")}
@@ -194,8 +194,8 @@ This method is used for setting the maximum cache size in bytes that can be used
 Method is used for fetching the required data from the backend or a web service using AJAX call and is then used for executing the callback passed to the cacheObject, it also executes the callbacks which have been queued after the ajax call was made and before it returned with the data.
 
 @method getObject
-@param String id is the hash Cache Object id (URL with params)
-@param Function callback is a callback function which gets executed once data is available
+@param {String} id is the hash Cache Object id (URL with params)
+@param {Function} callback is a callback function which gets executed once data is available
 **/
     getObject = function (id,callback) {
 	if(log){console.log("Sending AJAX request to fetch requested data")}
@@ -222,7 +222,7 @@ Method is used for fetching the required data from the backend or a web service 
  This method is used for removing a cacheObject based on the id passed to it.
 
 @method removeCacheObject
-@param String id is the hash id of the cacheObject (URL with params)
+@param {String} id is the hash id of the cacheObject (URL with params)
 **/
     removeCacheObject = function(id) {
 	if(log){console.log("Removing Cache Object id: ",id)}
@@ -234,9 +234,9 @@ Method is used for fetching the required data from the backend or a web service 
  Method is used for creating a template cache object and then adding data returned to it from the ajax call and then setting up its size and checks for Cahce memory overflow. If overflow then warning is given and then a particular algorithm is executed to balance the memory. After this balancing operation cacheObject is added to the cache and is returned as well.
 
 @method addCacheObject
-@param String id this is the hash id of the object (URL with params)
-@param Object data this is the data that has been returned from the ajax call.
-@return Object cacheObject is the fully formed object that contains flag data and the ajax data.
+@param {String} id this is the hash id of the object (URL with params)
+@param {Object} data this is the data that has been returned from the ajax call.
+@return {Object} cacheObject is the fully formed object that contains flag data and the ajax data.
 **/
     addCacheObject = function (id,data) {
 	if(log){console.log("Adding new cache object to the Cache Table")}
@@ -257,7 +257,7 @@ Method is used for fetching the required data from the backend or a web service 
 This is a Object replacement algorithm that removes any object that has been fetched the least so that new objects can take its place and cache is made useful and efficient.
 
 @method removeLeastFetched
-@param Number o_size is the size of the object that needs to be added to cache so that replacement of one or more cache objects can be carried out till the required size is freed in the cache
+@param {Number} o_size is the size of the object that needs to be added to cache so that replacement of one or more cache objects can be carried out till the required size is freed in the cache
 **/
     removeLeastFetched = function (o_size) {
 	if(log){console.log("Calling remove least fetched algoritm")}
@@ -276,7 +276,7 @@ This is a Object replacement algorithm that removes any object that has been fet
 This Cache Object replacement algorithm replaces objects by criterion as to which was the object that was fetched very long ago and has not been fetched again. All cache objects are updated with the latest fetch time and the oldest one is replaced.
 
 @method removeLeastUsed
-@param Number o_size is the size of the object that needs to be added to the cache
+@param {Number} o_size is the size of the object that needs to be added to the cache
 **/
     removeLeastUsed = function (o_size) {
 	if(log){console.log("calling remove least used algorithm")}
@@ -294,7 +294,7 @@ This Cache Object replacement algorithm replaces objects by criterion as to whic
 This replacement algorithm reoves the biggest cache object to make way for many smaller objects, it needs to be used in conjunction with the least used algorithm fir best results
 
 @method removeBiggest
-@param Number o_size is the size of the object that needs to be added into the cache.
+@param {Number} o_size is the size of the object that needs to be added into the cache.
 **/
     removeBiggest = function (o_size) {
 	if(log){console.log("calling remove biggest algorithm")}
